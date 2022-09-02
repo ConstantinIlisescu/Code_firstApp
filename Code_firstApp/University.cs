c65ecf8648c286efad373c3864a9debd491e06a7
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-09-02 12:40:32 +0100

    Add the University Class

---
+++ Code_firstApp/University.cs
@@ -0,0 +1,10 @@
+﻿namespace Code_firstApp
+{
+    internal class University
+    {
+        public int UniversityID { get; set; }
+        public string? UniversityName { get; set; }
+
+        public ICollection<Student>? Students { get; set; }
+    }
+}
