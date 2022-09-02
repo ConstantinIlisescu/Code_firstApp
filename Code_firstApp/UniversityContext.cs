118ff748898d4fbd35849c2fb100ae9c827590b2
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-09-02 12:45:01 +0100

    Add the UniversityContext class that derives from DbContext class

---
+++ Code_firstApp/UniversityContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace Code_firstApp
+{
+    internal class UniversityContext : DbContext
+    {
+        public UniversityContext() : base()
+        {
+
+        }
+
+        public DbSet<Student>? Students { get; set; }
+        public DbSet<University>? Universities { get; set; }
+    }
+}
