2ef9945f664a30778bf49b039dcb7db60286bb0c
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-09-02 13:27:28 +0100

    Add Student context class that derives from DbContext

--- Code_firstApp/Program.cs
+++ Code_firstApp/Program.cs
@@ -1,19 +1,45 @@
-﻿internal class Program
+﻿using Code_firstApp;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        using (var db = new UniversityContext())
+        using (var db = new StudentContext())
         {
 
             Console.WriteLine("What is your first name?");
             string firstName = Console.ReadLine();
 
-            Console.WriteLine("\nWhat is your second name?");
-            string secondName = Console.ReadLine();
+            Console.WriteLine("\nWhat is your last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("\nWhat town are you living in?");
+            string townName = Console.ReadLine();
 
             Console.WriteLine("\nWhat university do you whant to register to?");
             string universityName = Console.ReadLine();
 
+            Console.WriteLine("\n\n");
+
+            var student = new Student
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Town = townName,
+                UniversityName = universityName
+            };
+
+            db.Students.Add(student);
+            db.SaveChanges();
+
+            var query = from b in db.Students
+                        orderby b.FirstName
+                        select b;
+
+            foreach (var item in query)
+            {
+                Console.WriteLine($"Student {item.FirstName} whants to go to the University \"{item.UniversityName}\".");
+            }
 
 
             Console.WriteLine("\n\n\nPress any key to exit!");
---
+++ Code_firstApp/StudentContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace Code_firstApp
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student>? Students { get; set; }
+    }
+
+}
